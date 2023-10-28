6c2f868ae0832325222396e0572dc5d54ac68c42
Abiluthfi <abiluthfi300@gmail.com>
2023-10-29 00:00:20 +0700

    utspwl

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Selamat datang di Surga Sepatu</h1>"Destinasi terbaik untuk menemukan gaya dan kenyamanan dalam setiap langkah Anda. Di Toko Sepatu kami, kami tidak hanya menawarkan sepatu, kami memberikan pengalaman yang melampaui mode. Dari
+      koleksi klasik yang elegan hingga tren terbaru yang paling bergaya, kami menghadirkan berbagai pilihan sepatu yang dirancang untuk memenuhi gaya hidup dinamis Anda. Nikmati pengalaman berbelanja yang menggembirakan di toko kami yang
+      menampilkan deretan sepatu berkualitas tinggi dari merek terkenal di seluruh dunia."
+    </div>
+  );
+};
+
+export default Home;
